d047c026ac95a609b2db533934f6a63687652dd9
hr-sobuj <sobujhd@gmail.com>
2023-09-21 10:25:04 +0600

    diatomic updated

---
+++ de.py
@@ -0,0 +1,35 @@
+"""
+#Author     : Rakin
+#Date       : --2022
+#Language   : Python (3.10.5)
+#IDE        : PyCharm
+       CSE, 2018, HSTU
+"""
+
+frequent_words = ['Th', 'is', 'a ', 'to', 'of', 'in', 'ed', 'oo', 'el', 'li']
+replace_chars = ['@', '$', '%', '^', '&', '*', '_', '+', ';', '|']
+
+
+def diatomic_encoding(message):
+    encoded_text = ""
+    length = len(message) - 1
+    i = 0
+
+    while i < length:
+        if i + 1 <= length:
+            temp = message[i] + message[i + 1]
+            if temp in frequent_words:
+                encoded_text = encoded_text + (replace_chars[frequent_words.index(temp)])
+                i = i + 2
+                continue
+            else:
+                encoded_text = encoded_text + message[i]
+        else:
+            encoded_text = encoded_text + message[i]
+        i = i + 1
+
+    print(encoded_text)
+
+
+msg = "This is a demo text to encoded"
+diatomic_encoding(msg)
--- diatomic.py
+++ diatomic.py
@@ -1,6 +1,7 @@
-frequent_words = ['Th', 'is', 'a ', 'to', 'of', 'in', 'ed', 'oo', 'el', 'li']
-replace_chars = ['@', '$', '%', '^', '&', '*', '_', '+', ';', '|']
+diatomic_dict = {}
 
+def add_diatomic_mapping(word, replacement):
+    diatomic_dict[word] = replacement
 
 def diatomic_encoding(message):
     encoded_text = ""
@@ -9,19 +10,36 @@ def diatomic_encoding(message):
 
     while i < length:
         if i + 1 <= length:
-            temp = message[i] + message[i + 1]
-            if temp in frequent_words:
-                encoded_text = encoded_text + (replace_chars[frequent_words.index(temp)])
-                i = i + 2
+            temp = message[i:i + 2]
+            if temp in diatomic_dict:
+                encoded_text += diatomic_dict[temp]
+                i += 2
                 continue
             else:
-                encoded_text = encoded_text + message[i]
+                encoded_text += message[i]
         else:
-            encoded_text = encoded_text + message[i]
-        i = i + 1
+            encoded_text += message[i]
+        i += 1
 
+    return encoded_text
+
+def load_mappings_from_file(mapping_file):
+    with open(mapping_file, 'r') as file:
+        lines = file.readlines()
+    
+    for line in lines:
+        parts = line.strip().split(',')
+        if len(parts) == 2:
+            add_diatomic_mapping(parts[0], parts[1])
+
+if __name__ == "__main__":
+    mapping_file_name = "mappings.txt"
+    input_file_name = "input.txt"
+
+    load_mappings_from_file(mapping_file_name)
+
+    with open(input_file_name, 'r') as input_file:
+        input_text = input_file.read()
+
+    encoded_text = diatomic_encoding(input_text)
     print(encoded_text)
-
-
-msg = "This is a demo text to encoded"
-diatomic_encoding(msg)
